282e49342e0d7553705facfe583ac9f957a01c21
Wammero <nazhmud1@yandex.ru>
2025-12-28 02:10:34 +0300

    end

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b"Hello from Effective Mobile!")
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8080):
+    server_address = ('0.0.0.0', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting httpd server on port {port}")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
